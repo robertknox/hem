aac8f1d50712ca15bf156efb3c8880740ee110ba
robertknox <robertselkirkknox@yahoo.com>
2020-04-24 00:00:10 -0700

    added image

--- author_site/author_site/urls.py
+++ author_site/author_site/urls.py
@@ -20,20 +20,28 @@ from django.conf.urls import include
 from rest_framework import routers
 from myapi import views as myapi_views 
 from users import views as user_views 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 #router.register(r'users', user_views.UserViewSet)
 #router.register(r'groups', user_views.GroupViewSet)
 
 urlpatterns = [
-    url('register', user_views.register, name='register'),
-    url('login',  auth_views.LoginView.as_view(template_name='users/login.html'),  name='login'),
-    url('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    url('author1', include('author1.urls')),
+    url('^register/', user_views.register, name='register'),
+    url('^profile/', user_views.profile, name='profile'),
+    url('^login/',  auth_views.LoginView.as_view(template_name='users/login.html'),  name='login'),
+    url('^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url('^author1', include('author1.urls')),
     url(r'', include('author1.urls')),
-    url(r'home', include('author1.urls')),
+    url(r'^home', include('author1.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^myapi/', include(router.urls )),
-    url('myapi/',include('myapi.urls')), 
+    url('^myapi/',include('myapi.urls')), 
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
